13ad60cfb9d0120ea2af49d3f0ff90309e4d3fad
hakaitech <shlok.parida@gmail.com>
2024-01-30 10:47:19 +0530

    chkpoint

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
+var merge = flag.Bool("merge", false, "merge csvs")
+
 func main() {
-	input_path := "/Data_source_dir"
-	output_path := "Lotsize2018.csv"
+	flag.Parse()
+	if *merge {
+		MergeCSV("LotSizesNEW.csv", "Lotsizes2018.csv", "Lotsizes2019.csv", "Lotsizes2020.csv", "Lotsizes2022.csv", "Lotsizes2023.csv")
+		return
+	}
+	input_path := "/mnt/md0/LotSizeData/"
+	output_path := "Lotsizes.csv"
 	f, err := os.Create(output_path)
 	if err != nil {
 		log.Fatal(err)
@@ -28,62 +37,77 @@ func main() {
 		}
 		return nil
 	})
-	log.Println("File Cache done, starting DB and loading...")
+	log.Println("File Cache done, starting...")
 	if e != nil {
 		log.Println(e)
 	}
-	LotSizes := make(map[string]float64)
-	for _, filepath := range paths {
-		date := strings.Split(filepath, "/")[len(strings.Split(filepath, "/"))-1]
-		date = date[2 : len(date)-4]
-		cur_file, err := os.Open(filepath)
-		if err != nil {
-			log.Println(err)
-			continue
+	var wg sync.WaitGroup
+	chan_records := [][]string{}
+	var mux sync.Mutex
+	fmt.Println("Files : ", len(paths))
+	for ctr, path := range paths {
+		// defer close(chan_records)
+		if ctr%120 == 0 && ctr != 0 {
+			fmt.Println("Waiting")
+			wg.Wait()
 		}
-		cur_reader := csv.NewReader(cur_file)
-		contents, _ := cur_reader.ReadAll()
-		for _, line := range contents {
-			inst_name := line[0]
-			inst_name = strings.ReplaceAll(inst_name, "FUTIDX", "")
-			inst_name = strings.ReplaceAll(inst_name, "FUTSTK", "")
-			inst_name = strings.ReplaceAll(inst_name, "OPTIDX", "")
-			inst_name = strings.ReplaceAll(inst_name, "OPTSTK", "")
-			if _, ok := LotSizes[fmt.Sprintf("%s_%s", date, inst_name)]; !ok {
+		wg.Add(1)
+		go func(filepath string) {
+			date := strings.Split(filepath, "/")[len(strings.Split(filepath, "/"))-1]
+			date = date[2 : len(date)-4]
+			dte := ConvDateToTS(date)
+			cur_file, err := os.Open(filepath)
+			if err != nil {
+				log.Println(err)
+				wg.Done()
+				return
+			}
+			cur_reader := csv.NewReader(cur_file)
+			cur_reader.Read()
+			contents, _ := cur_reader.ReadAll()
+			insts := map[string]bool{}
+			for _, line := range contents {
+				inst_name := line[0]
+				inst_name = strings.ReplaceAll(inst_name, "FUTIDX", "")
+				inst_name = strings.ReplaceAll(inst_name, "FUTSTK", "")
+				inst_name = strings.ReplaceAll(inst_name, "OPTIDX", "")
+				inst_name = strings.ReplaceAll(inst_name, "OPTSTK", "")
+				exp := inst_name[strings.LastIndex(inst_name, "-")-6 : strings.LastIndex(inst_name, "-")+5]
+				e := ConvDateToTS(exp)
+				inst_name = inst_name[:strings.LastIndex(inst_name, "-")-6]
+				// fmt.Println(inst_name)
 				j, _ := strconv.ParseFloat(line[9], 64)
 				k, _ := strconv.ParseFloat(line[10], 64)
-				if j == 0 || k == 0 {
+
+				if k == 0 || j == 0 {
+					// fmt.Println("Skipped j==0 or k==0")
 					continue
 				}
+
 				lotsize := j / k
-				if !strings.Contains(inst_name, "CE") && !strings.Contains(inst_name, "PE") {
-					LotSizes[fmt.Sprintf("%s_%s", date, inst_name)] = lotsize
+				if _, ok := insts[inst_name]; !ok {
+					// fmt.Println("new: ", date)
+
+					insts[inst_name] = true
+					temp := []string{
+						fmt.Sprint(dte),
+						inst_name,
+						fmt.Sprint(e),
+						fmt.Sprint(lotsize),
+					}
+					mux.Lock()
+					chan_records = append(chan_records, temp)
+					mux.Unlock()
 				} else {
-					pos := strings.LastIndex(inst_name, "-")
-					pos = pos + 5
-					LotSizes[fmt.Sprintf("%s_%s", date, inst_name[:pos])] = lotsize
+					continue
 				}
-
-			} else {
-				continue
 			}
-		}
+			wg.Done()
+		}(path)
 	}
-
-	for name, lotsize := range LotSizes {
-		date := ConvDateToTS(strings.Split(name, "_")[0])
-		name_split := (strings.Split(name, "_")[1])
-		e := ConvDateToTS(name_split[len(name_split)-11:])
-		inst := name_split[:len(name_split)-11]
-		temp := []string{
-			fmt.Sprint(date),
-			inst,
-			fmt.Sprint(e),
-			fmt.Sprint(lotsize),
-		}
-		output_writer.Write(temp)
-		output_writer.Flush()
-	}
+	wg.Wait()
+	output_writer.WriteAll(chan_records)
+	output_writer.Flush()
 }
 
 func ConvDateToTS(name string) int64 {
@@ -118,10 +142,85 @@ func ConvDateToTS(name string) int64 {
 		case "DEC":
 			name = strings.ReplaceAll(name, month, "12")
 		default:
-			log.Fatal("Error", month)
+			log.Fatal("Error", name)
 		}
 		inter, _ := time.Parse("02-01-2006", name)
 		return inter.Unix()
 	}
 
 }
+
+func MergeCSV(dest string, filepaths ...string) {
+	file, err := os.Create(dest)
+	if err != nil {
+		log.Println("Error in creating destination csv: ", err)
+		return
+	}
+	csvwriter := csv.NewWriter(file)
+	csvwriter.Write([]string{"date", "instrument", "expiry", "lotsize"})
+	csvwriter.Flush()
+	for _, filepath := range filepaths {
+		temp_file, err := os.Open(filepath)
+		if err != nil {
+			log.Println("Error in creating destination csv: ", err)
+			return
+		}
+		csvReader := csv.NewReader(temp_file)
+		datum, _ := csvReader.ReadAll()
+		csvwriter.WriteAll(datum)
+		csvwriter.Flush()
+	}
+}
+
+// LotSizes := make(map[string]float64)
+// for _, filepath := range paths {
+// 	date := strings.Split(filepath, "/")[len(strings.Split(filepath, "/"))-1]
+// 	date = date[2 : len(date)-4]
+// 	cur_file, err := os.Open(filepath)
+// 	if err != nil {
+// 		log.Println(err)
+// 		continue
+// 	}
+// 	cur_reader := csv.NewReader(cur_file)
+// 	contents, _ := cur_reader.ReadAll()
+// 	for _, line := range contents {
+// 		inst_name := line[0]
+// 		inst_name = strings.ReplaceAll(inst_name, "FUTIDX", "")
+// 		inst_name = strings.ReplaceAll(inst_name, "FUTSTK", "")
+// 		inst_name = strings.ReplaceAll(inst_name, "OPTIDX", "")
+// 		inst_name = strings.ReplaceAll(inst_name, "OPTSTK", "")
+// 		if _, ok := LotSizes[fmt.Sprintf("%s_%s", date, inst_name)]; !ok {
+// 			j, _ := strconv.ParseFloat(line[9], 64)
+// 			k, _ := strconv.ParseFloat(line[10], 64)
+// 			if j == 0 || k == 0 {
+// 				continue
+// 			}
+// 			lotsize := j / k
+// 			if !strings.Contains(inst_name, "CE") && !strings.Contains(inst_name, "PE") {
+// 				LotSizes[fmt.Sprintf("%s_%s", date, inst_name)] = lotsize
+// 			} else {
+// 				pos := strings.LastIndex(inst_name, "-")
+// 				pos = pos + 5
+// 				LotSizes[fmt.Sprintf("%s_%s", date, inst_name[:pos])] = lotsize
+// 			}
+
+// 		} else {
+// 			continue
+// 		}
+// 	}
+// }
+
+// for name, lotsize := range LotSizes {
+// 	date := ConvDateToTS(strings.Split(name, "_")[0])
+// 	name_split := (strings.Split(name, "_")[1])
+// 	e := ConvDateToTS(name_split[len(name_split)-11:])
+// 	inst := name_split[:len(name_split)-11]
+// 	temp := []string{
+// 		fmt.Sprint(date),
+// 		inst,
+// 		fmt.Sprint(e),
+// 		fmt.Sprint(lotsize),
+// 	}
+// 	output_writer.Write(temp)
+// 	output_writer.Flush()
+// }
